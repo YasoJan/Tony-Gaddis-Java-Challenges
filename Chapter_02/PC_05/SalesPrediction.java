8e8732a5e05a04c53641700134839331dad5bbd3
yasojan <yasinzahir98@gmail.com>
2026-05-01 14:27:17 -0700

    Finish pset 5

---
+++ Chapter_02/PC_05/SalesPrediction.java
@@ -0,0 +1,15 @@
+/**************************************************************************************************** 
+* Author: Yasin Zahir
+
+* Sales Prediction
+
+* 4/17/2026
+*****************************************************************************************************/
+
+public class SalesPrediction{
+  public static void main(String[] args){
+    System.out.println("-------------- Sales Prediction --------------");
+    System.out.println("")
+    System.out.println("-------------- Sales Prediction --------------");
+  }
+}
